671eb7f7409952f95f63ce8f434915aa05ffe74a
Juan Pablo Garcia Blanco <juan1016gaba@gmail.com>
2026-09-24 12:05:03 -0500

    Initial commit, se crea el proyecto con Spring Initializr y se genera nuevo repositorio

---
+++ src/main/java/com/demo/datosmedicos/DatosmedicosApplication.java
@@ -0,0 +1,13 @@
+package com.demo.datosmedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatosmedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatosmedicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/datosmedicos/DatosmedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.datosmedicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatosmedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
